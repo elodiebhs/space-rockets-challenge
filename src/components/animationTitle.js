7ce598cc61b08a374afe3a822400b84db217059c
elodiebhs <elodiebouthors@hotmail.com>
2021-10-18 12:40:41 -0700

    add animation to title

---
+++ src/components/animationTitle.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import { useState } from "react";
+import { useSpring, animated, config } from 'react-spring'
+import { Text } from "@chakra-ui/core";
+
+
+export default function Animation() {
+  const [flip, set] = useState(false)
+  const props = useSpring({
+    to: [
+      { opacity: 1, color: '#white' },
+      { opacity: 0, color: '#0000ff' },
+    ],
+    from: { opacity: 0, color: '4d4dff' },
+    reset: true,
+    reverse: flip,
+    delay: 15,
+    config: config.molasses,
+    onRest: () => set(!flip),
+  })
+  return <animated.div style={props}>
+    <Text
+      fontFamily="mono"
+      letterSpacing="2px"
+      fontWeight="bold"
+      fontSize="lg"
+    >
+      ¡SPACE·R0CKETS!
+</Text>
+  </animated.div>
+}
--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Flex, Text } from "@chakra-ui/core";
+import { Flex} from "@chakra-ui/core";
 
 import Launches from "./launches";
 import Launch from "./launch";
 import Home from "./home";
 import LaunchPads from "./launch-pads";
 import LaunchPad from "./launch-pad";
+import Animation from "./animationTitle"
 
 export default function App() {
   return (
@@ -34,14 +35,8 @@ function NavBar() {
       bg="gray.800"
       color="white"
     >
-      <Text
-        fontFamily="mono"
-        letterSpacing="2px"
-        fontWeight="bold"
-        fontSize="lg"
-      >
-        ¡SPACE·R0CKETS!
-      </Text>
+      <Animation />
+
     </Flex>
   );
 }
