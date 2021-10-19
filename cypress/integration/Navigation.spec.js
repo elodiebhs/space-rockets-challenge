1b42deceb495eb5c7ead4929d352e52ede012103
elodiebhs <elodiebouthors@hotmail.com>
2021-10-19 15:58:06 -0700

    add navigation tests

--- cypress/integration/Navigation.spec.js
+++ cypress/integration/Navigation.spec.js
@@ -1,5 +1,17 @@
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("http://localhost:3002/");
+    cy.contains('Browse SpaceX Launches').click()
+    
   });
 });
+
+
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("http://localhost:3002/");
+    cy.contains('Browse SpaceX Launch Pads').click()
+    
+  });
+});
+
